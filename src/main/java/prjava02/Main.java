f1ecd3a0825e652cace6e1d4594e93540214afee
Jiongze <jiongzexu52+ioc@gmail.com>
2021-05-16 15:26:40 +0200

    Treballant amb ordres Git i branques: Versió 1.2 del projecte prjava02

--- src/main/java/prjava02/Main.java
+++ src/main/java/prjava02/Main.java
@@ -19,6 +19,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("versió 0.1 del projecte prjava02"); 	
         System.out.println("Creacio? d'una branca del projecte prjava02");
+        System.out.println("Afegint me?s codi a la branca00 del projecte prjava02");
         try {
             InetAddress adreca = InetAddress.getLocalHost();
             String hostname = adreca.getHostName();
